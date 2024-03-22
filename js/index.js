180e3f05add856d22357c15e39f44c66a3f1c6a4
ziyodilloxon <ziyodilloxon8@gmail.com>
2024-03-22 12:44:07 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,41 @@
+// 1 misol
+// let numbers = [1, 45, 23, 98, 3, 7];
+// let kvadrat= numbers.map((num) => num * num);
+// console.log(kvadrat);
+
+// 2 misol
+// let numbers = [5, 4, 3, 2, 1, 0, -1, -2, -3, -4, -5];
+// let salbiyraqamlar = numbers.filter((num) => num < 0);
+// console.log(salbiyraqamlar);
+
+// 3 misol
+// let arr = [1, 2, [3, 4], 5, [6, 7]];
+// let ichkiArray = arr.filter((element) => Array.isArray(element));
+// console.log(ichkiArray);
+
+// 9 misol
+// function arr(length) {
+//   let result = [];
+//   for (let i = 1; i <= length; i++) {
+//     result.push("x".repeat(i));
+//   }
+//   return result;
+// }
+// console.log(arr(4));
+
+// 10 misol
+// function arr(length) {
+//   let result = [];
+//   for (let i = 1; i <= length; i++) {
+//     result.push(String(i).repeat(i));
+//   }
+//   return result;
+// }
+// console.log(arr(7));
+
+// 8 misol
+// function arrayFill(value, length) {
+//   return Array.from({ length: length }, () => value);
+// }
+
+// console.log(arrayFill("y", 9));
